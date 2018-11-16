2708719056a37801f5611e05b32fa8db0822c1f4
Danny Kim <ssm_sebiya@naver.com>
2018-11-17 01:59:53 +0900

    fix instagram login dialog not display issue.

--- library/src/main/java/com/android/sebiya/net/social/AuthDialogFragment.java
+++ library/src/main/java/com/android/sebiya/net/social/AuthDialogFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import com.android.sebiya.net.R;
@@ -18,7 +19,6 @@ public abstract class AuthDialogFragment<T> extends DialogFragment {
 
     private AuthCallbacks<T> mAuthCallbacks;
     private View mProgress;
-    private WebView mWebView;
 
     protected abstract String getAuthUrl();
     protected abstract boolean shouldOverrideUrlLoading(WebView webView, String url, AuthCallbacks<T> callback);
@@ -31,19 +31,19 @@ public abstract class AuthDialogFragment<T> extends DialogFragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle bundle) {
         super.onViewCreated(view, bundle);
-        this.mWebView = view.findViewById(R.id.webView);
-        this.mProgress = view.findViewById(R.id.progress);
+        final WebView webView = view.findViewById(R.id.webView);
+        mProgress = view.findViewById(R.id.progress);
         if (getArguments() == null) {
             throw new IllegalArgumentException("this dialog should include argument clientId and redirect url");
         }
         getDialog().setCanceledOnTouchOutside(false);
         if (getActivity() instanceof AuthCallbacks) {
-            this.mAuthCallbacks = (AuthCallbacks) getActivity();
+            mAuthCallbacks = (AuthCallbacks) getActivity();
         }
-        if (this.mAuthCallbacks == null && (getParentFragment() instanceof AuthCallbacks)) {
-            this.mAuthCallbacks = (AuthCallbacks) getParentFragment();
+        if (mAuthCallbacks == null && (getParentFragment() instanceof AuthCallbacks)) {
+            mAuthCallbacks = (AuthCallbacks) getParentFragment();
         }
-        if (this.mAuthCallbacks == null) {
+        if (mAuthCallbacks == null) {
             throw new IllegalArgumentException("activity should implements auth callbacks");
         }
 
@@ -55,11 +55,11 @@ public abstract class AuthDialogFragment<T> extends DialogFragment {
             }
         });
         instance.flush();
-        this.mWebView.setHorizontalScrollBarEnabled(false);
-        this.mWebView.getSettings().setTextZoom(100);
-        this.mWebView.getSettings().setJavaScriptEnabled(true);
-        this.mWebView.setBackgroundColor(-1);
-        this.mWebView.setWebViewClient(new WebViewClient() {
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.getSettings().setTextZoom(100);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setBackgroundColor(-1);
+        webView.setWebViewClient(new WebViewClient() {
             public void onPageFinished(WebView webView, String str) {
                 super.onPageFinished(webView, str);
                 mProgress.setVisibility(View.GONE);
@@ -72,7 +72,9 @@ public abstract class AuthDialogFragment<T> extends DialogFragment {
                 return super.shouldOverrideUrlLoading(webView, url);
             }
         });
-        this.mWebView.loadUrl(getAuthUrl());
+        WebSettings settings = webView.getSettings();
+        settings.setDomStorageEnabled(true);
+        webView.loadUrl(getAuthUrl());
     }
 
     public void onDismiss(DialogInterface dialogInterface) {
@@ -80,6 +82,15 @@ public abstract class AuthDialogFragment<T> extends DialogFragment {
         if (getAuthResult() == null && mAuthCallbacks != null) {
             mAuthCallbacks.onAuthFailure();
         }
+
+        CookieManager instance = CookieManager.getInstance();
+        instance.removeAllCookies(new ValueCallback<Boolean>() {
+            @Override
+            public void onReceiveValue(final Boolean value) {
+
+            }
+        });
+        instance.flush();
     }
 
 }
